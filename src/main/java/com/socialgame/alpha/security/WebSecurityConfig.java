63e1fc327bd65e50de0b2ee8f3b3e2cf37a755ae
Ian Hupkes <ScotBlack@users.noreply.github.com>
2021-03-24 14:08:18 +0100

    -added acces to "/test/..."

---
+++ src/main/java/com/socialgame/alpha/security/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.socialgame.alpha.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity( prePostEnabled = true )
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+//                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/test/**").permitAll()
+                .anyRequest().authenticated();
+
+//        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
