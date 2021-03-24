c433371748bda5ce3c5296fb1b6a003af6185b1f
Ian Hupkes <ScotBlack@users.noreply.github.com>
2021-03-24 13:57:19 +0100

    -added players @PlayerConfig -printPlayers @PlayerController

--- src/main/java/com/socialgame/alpha/controller/PlayerController.java
+++ src/main/java/com/socialgame/alpha/controller/PlayerController.java
@@ -1,8 +1,14 @@
 package com.socialgame.alpha.controller;
 
+import com.socialgame.alpha.domain.Player;
 import com.socialgame.alpha.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PlayerController {
@@ -11,4 +17,12 @@ public class PlayerController {
 
     @Autowired
     public void setPlayerRepository(PlayerRepository playerRepository) {this.playerRepository = playerRepository;}
+
+
+    @GetMapping (path="/players")
+    public ResponseEntity<?> printPlayers () {
+        List<Player> players = playerRepository.findAll();
+
+        return new ResponseEntity<> (players, HttpStatus.OK);
+    }
 }
