0a050bb4a44266d3a987c96ef3fe21691fc4db41
Ian Hupkes <ScotBlack@users.noreply.github.com>
2021-04-12 23:33:56 +0200

    - - TeamAnswerRequest (still needs security for avoiding get points multiple times per MiniGame)

--- src/main/java/com/socialgame/alpha/controller/TeamController.java
+++ src/main/java/com/socialgame/alpha/controller/TeamController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/team")
 public class TeamController {
 
     private TeamService teamService;
