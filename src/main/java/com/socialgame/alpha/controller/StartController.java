3028d47a0830fc23d3c78177bc3367897c628ea8
Ian Hupkes <ScotBlack@users.noreply.github.com>
2021-05-07 13:26:14 +0200

    - Auth -> Start Controller & Service

--- src/main/java/com/socialgame/alpha/controller/StartController.java
+++ src/main/java/com/socialgame/alpha/controller/StartController.java
@@ -2,52 +2,52 @@ package com.socialgame.alpha.controller;
 
 import com.socialgame.alpha.dto.request.CreateGameRequest;
 import com.socialgame.alpha.dto.request.JoinGameRequest;
-import com.socialgame.alpha.service.AuthorizationServiceImpl;
+import com.socialgame.alpha.dto.response.ErrorResponse;
+import com.socialgame.alpha.service.StartServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
-public class AuthController {
+public class StartController {
 
     @Autowired
-    AuthorizationServiceImpl authorizationService;
+    StartServiceImpl startService;
 
     @PostMapping("/creategame")
     public ResponseEntity<?> createGame(@Valid @RequestBody CreateGameRequest createGameRequest) {
-        return authorizationService.createGame(createGameRequest);
+        return startService.createGame(createGameRequest);
     }
 
     @PostMapping("/joingame")
     public ResponseEntity<?> joinGame(@RequestBody JoinGameRequest joinGameRequest) {
-        return authorizationService.joinGame(joinGameRequest);
+        return startService.joinGame(joinGameRequest);
     }
 
     @PostMapping("/rejoin")
     public ResponseEntity<?> rejoinGame(@RequestBody JoinGameRequest joinGameRequest) {
-        return authorizationService.rejoin(joinGameRequest);
+        return startService.rejoin(joinGameRequest);
     }
+
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+
         ex.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
+            String errorType = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
+            errorResponse.addError(errorType, errorMessage);});
+
+        return ResponseEntity.status(400).body(errorResponse);
     }
 }
---
+++ src/test/java/com/socialgame/alpha/controller/StartControllerTest.java
@@ -0,0 +1,53 @@
+package com.socialgame.alpha.controller;
+
+import com.socialgame.alpha.service.TestService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class StartControllerTest {
+
+    @Mock
+    TestService testService;
+
+    @InjectMocks
+    StartController startController;
+
+    MockMvc mvc;
+
+    @BeforeEach
+    void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(startController).build();
+    }
+
+    @Test
+    void contextLoads() {
+        Assertions.assertNotNull(startController);
+    }
+
+
+    @Test
+    void createGame() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+
+    @Test
+    void rejoinGame() {
+    }
+
+    @Test
+    void handleValidationExceptions() {
+    }
+}
