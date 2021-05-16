2184ed97cb76a05ee3e47ab9966af1fb27450f62
Ian Hupkes <ScotBlack@users.noreply.github.com>
2021-05-16 16:36:35 +0200

    - Minor Tweaks

--- src/main/java/com/socialgame/alpha/service/UserDetailsServiceImpl.java
+++ src/main/java/com/socialgame/alpha/service/UserDetailsServiceImpl.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class
+UserDetailsServiceImpl implements UserDetailsService {
 
     UserRepository userRepository;
 
