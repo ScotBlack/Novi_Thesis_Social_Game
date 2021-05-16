af403bf06e0b7cbb73b38d2564cb162569334010
Ian Hupkes <ScotBlack@users.noreply.github.com>
2021-05-16 20:15:56 +0200

    - StartService 70% except AuthenticatseUser

--- src/test/java/com/socialgame/alpha/service/StartServiceImplTest.java
+++ src/test/java/com/socialgame/alpha/service/StartServiceImplTest.java
@@ -1,38 +1,34 @@
 package com.socialgame.alpha.service;
 
 import com.socialgame.alpha.configuration.security.jwt.JwtUtils;
+import com.socialgame.alpha.domain.Game;
+import com.socialgame.alpha.domain.Lobby;
+import com.socialgame.alpha.domain.Player;
 import com.socialgame.alpha.domain.Role;
-import com.socialgame.alpha.domain.User;
 import com.socialgame.alpha.domain.enums.ERole;
 import com.socialgame.alpha.dto.request.CreateGameRequest;
-import com.socialgame.alpha.dto.response.ErrorResponse;
-import com.socialgame.alpha.dto.response.JwtResponse;
+import com.socialgame.alpha.dto.request.JoinGameRequest;
 import com.socialgame.alpha.repository.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.servlet.http.HttpServletRequest;
-import java.nio.file.attribute.UserPrincipal;
+import javax.persistence.EntityNotFoundException;
+import java.util.HashSet;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.mockito.BDDMockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
 public class StartServiceImplTest {
@@ -41,19 +37,7 @@ public class StartServiceImplTest {
     private final StartServiceImpl startService = new StartServiceImpl();
 
     @Mock
-    private AuthenticationManager authenticationManager;
-
-    @Mock
-    private JwtUtils jwtUtils;
-
-    @Mock
-    PasswordEncoder encoder;
-
-    @Mock
     RoleRepository roleRepository;
-
-    @Mock
-    private PlayerRepository playerRepository;
 
     @Mock
     private GameRepository gameRepository;
@@ -61,28 +45,23 @@ public class StartServiceImplTest {
     @Mock
     private LobbyRepository lobbyRepository;
 
-    @Mock
-    private UserRepository userRepository;
+    Lobby lobby;
+    Game game;
 
-    @Mock
-    private UserDetailsImpl userDetailsImpl;
+    JoinGameRequest joinRequest;
 
-    @Mock
-    private Authentication authentication;
-
-    AuthenticationManager mockAuthenticationManager;
-    Authentication mockAuthentication;
-    UserDetailsImpl mockUserDetails;
-
+    Role gameHostRole = new Role(ERole.ROLE_GAMEHOST);
+    Role playerRole = new Role(ERole.ROLE_PLAYER);
 
     @BeforeEach
     void setUp() {
-//        request = mock(HttpServletRequest.class);
-//        mockPrincipal = mock(UserPrincipal.class);
-//
-//        when(request.getUserPrincipal()).thenReturn(mockPrincipal);
-//        when(mockPrincipal.getName()).thenReturn("player1");
-//        when(userRepository.findByUsername("player1")).thenReturn(Optional.ofNullable(user));
+        lobby = new Lobby();
+        game = new Game();
+        game.setStarted(false);
+
+        joinRequest = new JoinGameRequest();
+        joinRequest.setUsername("test");
+        joinRequest.setGameIdString("abc");
     }
 
 //    @Test
@@ -101,6 +80,7 @@ public class StartServiceImplTest {
 //        JwtResponse jwt = startService.authenticateUser("test", "test");
 //    }
 
+    /** generate gameIdString test */
     @RepeatedTest(100)
     void generateGameIdString_shouldNeverReturnPlaceholder() {
         String gameIdString = startService.generateGameIdString();
@@ -110,26 +90,12 @@ public class StartServiceImplTest {
         assertNotEquals("placeholder", gameIdString);
     }
 
-    @Test
-    void existingUserName_shouldReturnErrorResponse() {
-        CreateGameRequest request = new CreateGameRequest();
-        request.setUsername("takenUsername");
 
-        given(startService.generateGameIdString()).willReturn("abc");
-        when(userRepository.existsByUsername("abc_takenUsername")).thenReturn(Boolean.TRUE);
-
-    }
-
+    /** initializeGame Tests */
     @Test
     void createGame_shouldReturnGameIdString() {
-
-//        AuthenticationManager authenticationManager=mock(AuthenticationManager.class);
-        Role gameHostRole = new Role(ERole.ROLE_GAMEHOST);
-        Role playerRole = new Role(ERole.ROLE_PLAYER);
-
         CreateGameRequest request = new CreateGameRequest();
         request.setUsername("test");
-
 
         when(roleRepository.findByName(ERole.ROLE_GAMEHOST)).thenReturn(Optional.of(gameHostRole));
         when(roleRepository.findByName(ERole.ROLE_PLAYER)).thenReturn(Optional.of(playerRole));
@@ -149,7 +115,61 @@ public class StartServiceImplTest {
         assertEquals(3, gameIdString.length());
     }
 
+    /** initializePlayer test */
+    @Test
+    void notExistingLobby_shouldThrowException() {
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> {
+            startService.initializePlayer(joinRequest);
+        });
 
+        String expectedMessage = "Lobby with: " + joinRequest.getGameIdString() + " does not exist.";
+
+        assertTrue(exception.getMessage().contains(expectedMessage));
+    }
+
+    @Test
+    void notExistingGame_shouldThrowException() {
+        when(lobbyRepository.findByGameIdString(joinRequest.getGameIdString())).thenReturn(Optional.of(lobby));
+
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> {
+            startService.initializePlayer(joinRequest);
+        });
+
+        String expectedMessage = "Game with: " + joinRequest.getGameIdString() + " does not exist.";
+
+        assertTrue(exception.getMessage().contains(expectedMessage));
+    }
+
+    @Test
+    void joiningStartedGame_shouldThrowException() {
+        game.setStarted(true);
+
+        when(lobbyRepository.findByGameIdString(joinRequest.getGameIdString())).thenReturn(Optional.of(lobby));
+        when(gameRepository.findByGameIdString(joinRequest.getGameIdString())).thenReturn(Optional.of(game));
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            startService.initializePlayer(joinRequest);
+        });
+
+        String expectedMessage = "You can't join, game has already started.";
+
+        assertTrue(exception.getMessage().contains(expectedMessage));
+    }
+
+    @Test
+    void secondPlayerInLobby_shouldSetPlayerColorBlue() {
+        String expectedUsername = joinRequest.getGameIdString() + "_" + joinRequest.getUsername();
+
+        lobby.setPlayers(new HashSet<>());
+
+        when(lobbyRepository.findByGameIdString(joinRequest.getGameIdString())).thenReturn(Optional.of(lobby));
+        when(gameRepository.findByGameIdString(joinRequest.getGameIdString())).thenReturn(Optional.of(game));
+        when(roleRepository.findByName(ERole.ROLE_PLAYER)).thenReturn(Optional.of(playerRole));
+
+        String username = startService.initializePlayer(joinRequest);
+
+        assertEquals(username, expectedUsername);
+    }
 }
 
 
